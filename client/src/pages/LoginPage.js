2c7809fb88264841353e6791fc3429032869fb84
million-dollar-dev <hoangtuan1304@hotmail.com>
2024-06-09 08:10:40 +0700

    feat: Add show/hide password visibility in login form

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
-import {FiEye} from "react-icons/fi";
+import {FaRegEye, FaRegEyeSlash} from "react-icons/fa";
 
 const LoginPage = () => {
-    const showPassword = true;
+    const [visible, setVisible] = useState(false);
     return (
         <div className='mt-5'>
             <div className='bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4 mx-auto'>
@@ -12,12 +12,12 @@ const LoginPage = () => {
                 </h2>
                 <div className="grid gap-4 mt-5">
                     <div className="flex flex-col gap-1">
-                        <label htmlFor='name'>Name :</label>
+                        <label htmlFor='name'>Username :</label>
                         <input
                             type='text'
                             id='name'
                             name='name'
-                            placeholder='Enter your name'
+                            placeholder='Enter your username'
                             className='bg-slate-100 px-2 py-1 focus:outline-primary'
                             required
                         />
@@ -26,16 +26,19 @@ const LoginPage = () => {
                         <label htmlFor='email'>Password :</label>
                         <div className="flex items-center relative">
                             <input
-                                type={showPassword ? 'text' : 'password'}
+                                type={visible ? 'text' : 'password'}
                                 id='password'
                                 name='password'
                                 placeholder='Enter your password'
                                 className='flex-1 bg-slate-100 px-2 py-1 focus:outline-primary pr-12 rounded'
                                 required
                             />
-                            <div className="text-xl p-2 absolute top-0.2 right-2">
-                                <FiEye/>
-                            </div>
+                            <button
+                                className="text-xl p-2 absolute top-0.2 right-2"
+                                onClick={() => setVisible(!visible)}
+                            >
+                                {visible ? (<FaRegEyeSlash />) : (<FaRegEye />) }
+                            </button>
                         </div>
                     </div>
                     <button
