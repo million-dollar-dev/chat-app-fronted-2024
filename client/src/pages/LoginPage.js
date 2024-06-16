df9e1c1ad755d6317780a2a53a057d0efd73b881
million-dollar-dev <hoangtuan1304@hotmail.com>
2024-06-16 17:41:08 +0700

    feat: add login handler

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,45 @@
 import React, {useState} from 'react';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {FaRegEye, FaRegEyeSlash} from "react-icons/fa";
+import toast from "react-hot-toast";
+import websocketService from "../services/websocket";
 
 const LoginPage = () => {
     const [visible, setVisible] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const loginData = {
+            "action": "onchat",
+            "data": {
+                "event": "LOGIN",
+                "data": {
+                    "user": username,
+                    "pass": password
+                }
+            }
+        };
+
+        websocketService.send(loginData);
+
+        websocketService.socket.onmessage = (message) => {
+            const response =  JSON.parse(message.data);
+            console.log(response);
+            if (response.status === 'success') {
+                navigate('/');
+                toast.success('Login successfully');
+            } else {
+                setPassword('');
+                toast.error(response.mes);
+            }
+        };
+    };
+
     return (
         <div className='mt-5'>
             <div className='bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4 mx-auto'>
@@ -17,9 +53,12 @@ const LoginPage = () => {
                             type='text'
                             id='name'
                             name='name'
+                            required
                             placeholder='Enter your username'
                             className='bg-slate-100 px-2 py-1 focus:outline-primary'
-                            required
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+
                         />
                     </div>
                     <div className="flex flex-col gap-1 ">
@@ -29,20 +68,24 @@ const LoginPage = () => {
                                 type={visible ? 'text' : 'password'}
                                 id='password'
                                 name='password'
-                                placeholder='Enter your password'
-                                className='flex-1 bg-slate-100 px-2 py-1 focus:outline-primary pr-12 rounded'
                                 required
+                                placeholder='Enter your password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className='flex-1 bg-slate-100 px-2 py-1 focus:outline-primary pr-12 rounded'
+
                             />
                             <button
                                 className="text-xl p-2 absolute top-0.2 right-2"
                                 onClick={() => setVisible(!visible)}
                             >
-                                {visible ? (<FaRegEyeSlash />) : (<FaRegEye />) }
+                                {visible ? (<FaRegEyeSlash/>) : (<FaRegEye/>)}
                             </button>
                         </div>
                     </div>
                     <button
                         className='bg-primary text-lg px-4 py-1 hover:bg-secondary rounded mt-2 text-white leading-relaxed tracking-wide'
+                        onClick={handleLogin}
                     >
                         Login
                     </button>
